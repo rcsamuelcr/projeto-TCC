c17a5fcde362a5e26f904e535bd61a08b2c9af95
rcsamuelcr <samuelcostarodrigues2002@gmail.com>
2023-02-18 17:40:36 -0300

    Versão com o text area funcionando em JS, mas apenas o backgroun-image

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@ let btnNext = document.getElementById("next");
 let btnLeft = document.getElementById("btnLeft");
 let btnRight = document.getElementById("btnRight");
 let level = document.getElementById("level");
+let textarea = document.getElementById("code");
+
+let div = document.querySelector('.div-direita');
 let nivel = 1;
 
 // -------- Verificar se o código está certo antes de passar para a próxima fase  --------
-btnNext.addEventListener('click', () => {
-    console.log("FOIIIIIIIIIIIIIIIIIIIIIII");
+btnNext.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    code = textarea.value;
+    
+    div.style.backgroundImage =`${code}`;
+
+
+    console.log(code);
     
 })
 
